1f27de7c017b4f8f693aa4e9926981a33cb70917
mmadajski <80204566+mmadajski@users.noreply.github.com>
2022-05-10 20:53:06 +0200

    some changes

--- serwer/Server/Server.py
+++ serwer/Server/Server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import cv2 as cv2
 import numpy as np
+import dvc.api
 
 app = Flask(__name__)
 # git test changes 
@@ -21,6 +22,28 @@ def send_image():
         return "done"
     else:
         return "Something is not yes"
+
+"""
+@app.route('/dvc_update', methods = ["POST", "GET"])
+def dvc_update():
+    if request.method == "POST":
+        with dvc.api.open('Images.zip',repo='https://github.com/Ich-czworo/Mobile-Paragon/blob/MP-19-python-dvc/serwer/res/data/Images.zip.dvc') as images:
+            a = type(images)
+        return a
+    else:
+        return "xDDD"
+
+
+
+
+path = "C:/Users/48728/Desktop/fr_with_picts/pictures_2021-03-14-211101"
+valid_images = [".jpg",".gif",".png",".tga"]
+for f in os.listdir(path):
+    ext = os.path.splitext(f)[1]
+    if ext.lower() in valid_images:
+        new_row = {'img': os.path.join(path,f), 'img_name': os.path.basename(f),'path':path}
+        df_img = df_img.append(new_row, ignore_index = True)     
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
