a957502deaf0a986aff2a24f19d2b72f98a5138b
mmadajski <80204566+mmadajski@users.noreply.github.com>
2022-05-11 18:18:26 +0200

    code clean up

--- serwer/Server/Server.py
+++ serwer/Server/Server.py
@@ -4,7 +4,6 @@ import numpy as np
 import dvc.api
 
 app = Flask(__name__)
-# git test changes 
 
 @app.route('/', methods=["POST", "GET"])
 def serwer_run():
@@ -21,29 +20,7 @@ def send_image():
 
         return "done"
     else:
-        return "Something is not yes"
-
-"""
-@app.route('/dvc_update', methods = ["POST", "GET"])
-def dvc_update():
-    if request.method == "POST":
-        with dvc.api.open('Images.zip',repo='https://github.com/Ich-czworo/Mobile-Paragon/blob/MP-19-python-dvc/serwer/res/data/Images.zip.dvc') as images:
-            a = type(images)
-        return a
-    else:
-        return "xDDD"
-
-
-
-
-path = "C:/Users/48728/Desktop/fr_with_picts/pictures_2021-03-14-211101"
-valid_images = [".jpg",".gif",".png",".tga"]
-for f in os.listdir(path):
-    ext = os.path.splitext(f)[1]
-    if ext.lower() in valid_images:
-        new_row = {'img': os.path.join(path,f), 'img_name': os.path.basename(f),'path':path}
-        df_img = df_img.append(new_row, ignore_index = True)     
-"""
+        return "Image has not been send correctly"
 
 if __name__ == '__main__':
     app.run(debug=True)
