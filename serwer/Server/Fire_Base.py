cb5084cd3b01c5a105f609b28842c885a533699b
mmadajski <80204566+mmadajski@users.noreply.github.com>
2022-06-01 09:35:23 +0200

    Crud with firebase

--- serwer/Server/Fire_Base.py
+++ serwer/Server/Fire_Base.py
@@ -10,12 +10,21 @@ default_app = firebase_admin.initialize_app(cred, {
 	'databaseURL':"https://paragon-scanner-default-rtdb.firebaseio.com/"
 	})
 
-ref = db.reference("/key1/key2/")
+ref = db.reference("/key1/")
 data = 	ref.get()
-print(data)
+data2 = ref.child("key1").get()
+data3 = db.reference("/key1/").child("key3").get()
+data4 = ref.get()
+print("key2/: ", data)
+print("key2/.child: ", data2)
+print("data3: ", data3)
+print("data4: ", data4)
 
+ref2 = db.reference("/key1/key19")
+ref2.set(101)
 
-#cred_obj = firebase_admin.credentials.Certificate('C:\\Users\\micha\\Firebase\\paragon-scanner-firebase-adminsdk-n0fyt-1238f8bdb5.json')
-#default_app = firebase_admin.initialize_app(cred_obj, {
-	#'databaseURL':databaseURL
-	#})
+ref3 = db.reference("/1/")
+print("ref3:", ref3)
+query = ref3.order_by_key().get()
+
+print("query: ", query)
